30b8443b6ee7582f1b344f22e82b888189bbcbd2
Bruce Mayenschein <brucedmayenschein@gmail.com>
2023-02-27 11:58:41 -0500

    Character count solution

---
+++ 6kyu/CharacterCount.js
@@ -0,0 +1,15 @@
+// The main idea is to count all the occurring characters in a string. If you have a string like aba, then the result should be {'a': 2, 'b': 1}.
+
+// What if the string is empty? Then the result should be empty object literal, {}.
+
+function count(string) {  
+  let count = {};
+  for (let i =0; i < string.length; i++){
+    if (string[i] in count){
+      count[string[i]] += 1;
+    } else {
+      count[string[i]] = 1;
+    }
+   }
+  return count;
+}
